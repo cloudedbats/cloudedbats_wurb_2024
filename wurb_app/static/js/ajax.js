59a7e7a1f89cd98a4f92ae2e3a80fbbdcf47b4f5
Arnold Andreasson <arnold@mellifica.se>
2022-12-29 01:50:49 +0100

    Buttons inverted.

--- wurb_app/static/js/ajax.js
+++ wurb_app/static/js/ajax.js
@@ -1,11 +1,17 @@
 
 function fetchAjaxRecord() {
+    $("#ajax-record-id").addClass("is-inverted");
+    $("#ajax-live-id").addClass("is-inverted");
+    $("#ajax-annotations-id").addClass("is-inverted");
+    $("#ajax-files-id").addClass("is-inverted");
+
     $.ajax({
         // url: '/status/{{ uid }}',
         url: '/ajax-record/',
         type: 'get',
         success: function (data) {
-            $("#id-hero-body").html(data);
+            $("#hero-body-id").html(data);
+            $("#ajax-record-id").removeClass("is-inverted");
         },
         error: function (xhr, status, error) {
             var errorMessage = xhr.status + ': ' + xhr.statusText
@@ -15,12 +21,18 @@ function fetchAjaxRecord() {
 }
 
 function fetchAjaxLive() {
+    $("#ajax-record-id").addClass("is-inverted");
+    $("#ajax-live-id").addClass("is-inverted");
+    $("#ajax-annotations-id").addClass("is-inverted");
+    $("#ajax-files-id").addClass("is-inverted");
+
     $.ajax({
         // url: '/status/{{ uid }}',
         url: '/ajax-live/',
         type: 'get',
         success: function (data) {
-            $("#id-hero-body").html(data);
+            $("#hero-body-id").html(data);
+            $("#ajax-live-id").removeClass("is-inverted");
         },
         error: function (xhr, status, error) {
             var errorMessage = xhr.status + ': ' + xhr.statusText
@@ -30,12 +42,18 @@ function fetchAjaxLive() {
 }
 
 function fetchAjaxAnnotations() {
+    $("#ajax-record-id").addClass("is-inverted");
+    $("#ajax-live-id").addClass("is-inverted");
+    $("#ajax-annotations-id").addClass("is-inverted");
+    $("#ajax-files-id").addClass("is-inverted");
+
     $.ajax({
         // url: '/status/{{ uid }}',
         url: '/ajax-annotations/',
         type: 'get',
         success: function (data) {
-            $("#id-hero-body").html(data);
+            $("#hero-body-id").html(data);
+            $("#ajax-annotations-id").removeClass("is-inverted");
         },
         error: function (xhr, status, error) {
             var errorMessage = xhr.status + ': ' + xhr.statusText
@@ -45,12 +63,18 @@ function fetchAjaxAnnotations() {
 }
 
 function fetchAjaxFiles() {
+    $("#ajax-record-id").addClass("is-inverted");
+    $("#ajax-live-id").addClass("is-inverted");
+    $("#ajax-annotations-id").addClass("is-inverted");
+    $("#ajax-files-id").addClass("is-inverted");
+
     $.ajax({
         // url: '/status/{{ uid }}',
         url: '/ajax-files/',
         type: 'get',
         success: function (data) {
-            $("#id-hero-body").html(data);
+            $("#hero-body-id").html(data);
+            $("#ajax-files-id").removeClass("is-inverted");
         },
         error: function (xhr, status, error) {
             var errorMessage = xhr.status + ': ' + xhr.statusText
@@ -60,12 +84,17 @@ function fetchAjaxFiles() {
 }
 
 function fetchAjaxAbout() {
+    $("#ajax-record-id").addClass("is-inverted");
+    $("#ajax-live-id").addClass("is-inverted");
+    $("#ajax-annotations-id").addClass("is-inverted");
+    $("#ajax-files-id").addClass("is-inverted");
+
     $.ajax({
         // url: '/status/{{ uid }}',
         url: '/ajax-about/',
         type: 'get',
         success: function (data) {
-            $("#id-hero-body").html(data);
+            $("#hero-body-id").html(data);
         },
         error: function (xhr, status, error) {
             var errorMessage = xhr.status + ': ' + xhr.statusText
